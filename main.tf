148adc6c4fecd624ef96118e206d9a81c953c9de
PeytonDenisse <dcv78658@lasalle.edu.mx>
2024-11-12 12:05:13 -0600

    Proyecto de terraforms

---
+++ main.tf
@@ -0,0 +1,168 @@
+//achivo 1 
+
+//definir el provedor 
+provider "digitalocean" {
+    token = var.DO_TOKEN
+  
+}
+
+//definir nuestras variables 
+terraform {
+  required_providers {
+    digitalocean = {
+        source = "digitalocean/digitalocean"
+        version = "~> 2.0"
+        
+    }
+  }
+  backend "s3" {
+    endpoints = {
+      s3 = "https://sfo3.digitaloceanspaces.com"
+    }
+    bucket = "devdcv"
+    key = "terraform.tfstate"
+    skip_credentials_validation = true
+    skip_requesting_account_id = true
+    skip_metadata_api_check = true
+    skip_s3_checksum = true
+    region = "us-east-1"
+  }
+}
+
+//levantar recursos 
+//crear un proyecto desde aqui
+
+resource "digitalocean_project" "denisse_server_project" {
+  name = "denisse_server_project"
+  description = "un servidor para cositas personales"
+  resources = [ digitalocean_droplet.denisse_server_droplet.urn]
+}
+
+//crear llaves 
+resource "digitalocean_ssh_key" "denisse_server_ssh_key" {
+    name = "denisse_server_key2"
+    public_key = file("./keys/denisse_server.pub")
+  
+}
+
+
+//crear maquina virtual 
+
+resource "digitalocean_droplet" "denisse_server_droplet" {
+    name = "denisseserver"
+    size = "s-2vcpu-4gb-120gb-intel"
+    image = "ubuntu-24-04-x64"
+    region = "sfo3"
+    ssh_keys = [digitalocean_ssh_key.denisse_server_ssh_key.id]
+    user_data = file("./docker-install.sh")
+
+
+
+    provisioner "remote-exec" {
+       inline = [ 
+        "mkdir -p /projects",
+        "touch /projects/.env",
+        "echo \"MYSQL_DB=${var.MYSQL_DB}\" >> /projects/.env",
+        "echo \"MYSQL_USER=${var.MYSQL_USER}\" >> /projects/.env",
+        "echo \"MYSQL_PASSWORD=${var.MYSQL_PASSWORD}\" >> /projects/.env",
+        "echo \"MYSQL_HOST=${var.MYSQL_HOST}\" >> /projects/.env",
+        
+        ]
+      connection {
+        type = "ssh"
+        user = "root"
+        private_key = file("./keys/denisse_server")
+        host = self.ipv4_address
+      }
+    }
+
+    provisioner "file" {
+      source = "./container/docker-compose.yml"
+      destination = "/projects/docker-compose.yml"
+      connection {
+        type = "ssh"
+        user = "root"
+        private_key = file("./keys/denisse_server")
+        host = self.ipv4_address
+      }
+    }  
+}
+
+
+
+//instalar docker 
+
+# resource "time_sleep" "wait_docker_install" {
+#     depends_on = [ digitalocean_droplet.denisse_server_droplet ]
+#     create_duration = "130s"
+
+# }
+
+# resource "null_resource" "init_api" {
+#   depends_on = [ time_sleep.wait_docker_install ]
+#   provisioner "remote-exec" {
+#     inline = [ 
+#       "cd /projects",
+#       "docker-compose up -d",
+#      ]
+#      connection {
+#       type = "ssh"
+#       user = "root"
+#       private_key = file("./keys/denisse_server")
+#       host = digitalocean_droplet.denisse_server_droplet.ipv4_address
+#     }
+#   }
+# }
+
+//conectarse a ssh 
+# resource "null_resource" "init_nginx" {
+#   depends_on = [ time_sleep.wait_docker_install ]
+#   connection {
+#     type = "ssh"
+#     user = "root"
+#     private_key = file("./keys/denisse_server")
+#     host = digitalocean_droplet.denisse_server_droplet.ipv4_address
+#   }
+#   provisioner "remote-exec" {
+#     inline = [ "docker container run --name=Adidas -dp 80:80 nginx" ]
+#   }
+# }
+
+# //levantar el contenedor con nginx
+# resource "null_resource" "adidas_copy_folder2" {
+#   provisioner "file" {
+#     source = "./adidas"
+#     destination = "/adidas"
+#   }
+#   connection {
+#     type = "ssh"
+#     user = "root"
+#     private_key = file("./keys/denisse_server")
+#     host = digitalocean_droplet.denisse_server_droplet.ipv4_address
+#   }
+  
+# }
+
+# resource "null_resource" "copy_to_nginx_container" {
+#   depends_on = [ null_resource.adidas_copy_folder2, null_resource.init_nginx ]
+#   connection {
+#     type = "ssh"
+#     user = "root"
+#     private_key = file("./keys/denisse_server")
+#     host = digitalocean_droplet.denisse_server_droplet.ipv4_address
+#   }
+#   provisioner "remote-exec" {
+#     inline = [ 
+#       "cd /",
+#       "docker cp adidas/. Adidas:/usr/share/nginx/html"
+#      ]
+    
+#   }
+  
+# }
+
+
+output "ip" {
+  value = digitalocean_droplet.denisse_server_droplet.ipv4_address
+}
+
---
+++ variables.tf
@@ -0,0 +1,23 @@
+//archivo 2 
+//se crea un tokem y definimos us nombre  y la mandamos llammar 
+variable "DO_TOKEN" {
+    type = string
+  
+}
+
+variable "MYSQL_DB"{
+    type = string
+}
+
+variable "MYSQL_USER"{
+    type = string 
+  
+}
+
+variable "MYSQL_PASSWORD"{
+    type = string
+}
+
+variable "MYSQL_HOST"{
+    type = string
+}
